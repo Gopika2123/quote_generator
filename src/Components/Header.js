4e64e9c3ad193a2cb4c9294e002131f8219fe271
Gopika2123 <gopikaindira2001@gmail.com>
2023-03-26 11:40:45 +0530

    quote

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+
+// function Header() {
+//   return (
+//     <div className='nav'>
+//       <Navbar bg="dark" variant="dark">
+//         <Container>
+//           <Nav className="me-auto">
+//             <Nav.Link href="'/'">Home</Nav.Link>
+//             <Nav.Link href="/Bookmark">Bookmark</Nav.Link>
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//       <br />
+      
+//     </div>
+//   );
+// }
+
+// export default Header;
